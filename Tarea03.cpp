8d6345be73342b0a35b297ce910370b4558b77c3
joseacuna <joseacunaacuna@gmail.com>
2014-04-26 17:10:26 -0300

    el programa se ajusta al formato y se agrega la tercera consulta

--- Tarea03.cpp
+++ Tarea03.cpp
@@ -9,29 +9,25 @@
 #include<iostream>
 #include<cstring>
 #include<ctime>
+//librerias de postgresql
 #include<postgresql/libpq-fe.h>
+//librerias graficas
+#include<plplot/plplot.h>
+#include<plplot/plstream.h>
 
 using namespace std;
 
 /*
  * 
  */
-int main(int argc, char** argv) {
-    
+
+void MostrarConsulta(string queryDeSQL){
         PGconn *conexion;
         PGresult *res;
         int i,j;
         
-        //-----------
-        
-
-        //conexion a la base de datos
-        conexion= PQsetdbLogin("sebastian.cl","5432",NULL,NULL,"iswdb","isw","isw");
-        
-
-        string queryDeSQL="select tienda ,sum(monto) as Venta from ventas group by tienda;";
-        // comprobamos la conexion
-        if (PQstatus(conexion) != CONNECTION_BAD)
+  conexion= PQsetdbLogin("sebastian.cl","5432",NULL,NULL,"iswdb","isw","isw");
+ if (PQstatus(conexion) != CONNECTION_BAD)
             
 {
         cout << "\t\t--------------------------------------" << endl;
@@ -62,9 +58,51 @@ PQclear(res);
 }
 
 PQfinish(conexion);
-//Entrega La Fecha de Compilacion
-cout<<endl;
-cout << "Fecha de compilacion: " << __DATE__ << " " << __TIME__ << endl;
+}
+
+
+int main(int argc, char** argv) {
+    
+        
+        string queryDeSQL;
+        //-----------
+        // captura de las Opciones
+        string opcion1="-r";
+        string opcion2="-g tienda";
+        string opcion3="-bh";
+        string opcion4="-v";
+        for(int i = 1; i < argc; i++)
+            if(argv[1]==opcion1){
+                //conexion a la base de datos
+                
+                queryDeSQL="select tienda as TIENDA, date_part('hour' , fecha) as HORA, avg(monto) as promventa  from ventas  group by TIENDA , HORA order by hora asc";
+                MostrarConsulta(queryDeSQL);
+            }else{
+                if(argv[1]== opcion2){
+                }else{
+                    if(argv[1]==opcion3){
+                     
+                    queryDeSQL="select tienda, date_part('hour' , fecha) as hora,  avg(monto) as promedio from ventas group by tienda,hora"  
+                                  " having( avg(monto) > (select  avg(promventa) as promedio from(	select tienda, date_part('hour' , fecha) as hora, avg(monto) as promventa"  
+                                  " from ventas group by tienda,hora order by tienda,hora asc) as consulta))order by promedio desc;" ;
+                    MostrarConsulta(queryDeSQL);
+                    }else{
+                        if(argv[1]== opcion4){
+                       //Entrega La Fecha de Compilacion
+                        cout<<endl;
+                        cout << "Fecha de compilacion: " << __DATE__ << " " << __TIME__ << endl;
+                        cout<< "Grupo(8)"<<endl;
+                        cout<<"************************"<<endl;
+                        cout<< "José Acuña"<< endl << "Cristian Trujillo"<< endl<<"Luis Sandoval"<<endl;
+                        
+                        }
+            
+                    }
+                }
+            }
+        
+
+
 return 0;
 }
 
